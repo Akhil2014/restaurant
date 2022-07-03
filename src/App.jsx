564979bd6e1e7ab45c74af72b0c9bdbfad08aa41
Akhil Khan <akhilkhan0786khan@gmail.com>
2022-07-03 19:05:31 +0530

    filter

--- src/App.jsx
+++ src/App.jsx
@@ -6,10 +6,12 @@ import Navbar from "./Components/Navbar/Navbar";
 function App() {
   const [data, setData] = useState([]);
   const [page, setPage] = useState(1);
+  const [review, setReview] = useState(0)
+  const [sort,setSort] = useState("")
   const getData = async () => {
     try {
       const res = await fetch(
-        `http://localhost:8888/data?_page=${page}&_limit=6`
+        `http://localhost:8888/data?rating_gte=${review}&rating_lte=5&_page=${page}&_limit=6&sort=rating&_order=${sort}`
       );
       const data = await res.json();
       console.log(data);
@@ -21,23 +23,20 @@ function App() {
 
   useEffect(() => {
     getData();
-  }, [page]);
+  }, [page,review,sort]);
 
-  return (
-    <div className="App">
+      return ( 
+  <div className="App">
       <Navbar />
       <div className="body">
         <div className="sorting-button">
-          <button className="button1">1 Rating</button>
-          <button className="button1">2 Rating</button>
-          <button className="button1">3 Rating</button>
-          <button className="button1">4 Rating</button>
-          <button className="button1">Cash only</button>
-          <button className="button1">Card only</button>
-          <button className="button1">UPI only</button>
-          <button className="button1">Low to high</button>
-          <button className="button1">High to low</button>
-          <button className="button1">All</button>
+          <button onClick = {() => setReview(1)} className="button1">1 Rating</button>
+          <button onClick = {() => setReview(2)} className="button1">2 Rating</button>
+          <button onClick = {() => setReview(3)} className="button1">3 Rating</button>
+          <button onClick = {() => setReview(4.5)} className="button1">4 Rating</button>
+          <button onClick = {() => setSort("asc")} className="button1">Low to high</button>
+          <button onClick = {() => setSort("desc")} className="button1">High to low</button>
+          <button onClick = {() => setReview(0)} className="button1">All</button>
         </div>
         <div className="grid">
           {data.map((e) => (
